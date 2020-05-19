617d2e9cab92a4e44cad62a0677002ef23e44041
turok01 <44950560+turok01@users.noreply.github.com>
2020-05-19 17:02:58 +0300

    Initial commit

---
+++ src/main/java/com/example/SimpleRestController/SimpleRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleRestController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleRestController/SimpleRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleRestController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
